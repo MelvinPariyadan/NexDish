7b40305296c15b48a4c888563f389f6b30d26893
joygilna <gilnajoy007@gmail.com>
2025-05-19 00:46:23 +0200

    add test accuracy

---
+++ tests/test_accuracy.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+from dotenv import load_dotenv
+import os
+
+def test_model_accuracy_threshold():
+    load_dotenv()
+    path_to_model = os.getenv("PATH_TO_MODEL")
+    model_data = torch.load(path_to_model)
+    test_acc = model_data.get('test_acc', 0.0)
+
+    assert test_acc >= 0.85, f"Top-1 Accuracy too low: {test_acc:.2%}"
