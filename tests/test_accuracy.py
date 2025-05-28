0d9c7ab7a16e7597c0eb0f2a8d713fddcddcfaed
MelvinPariyadan <melvinpariyadan@gmail.com>
2025-05-28 21:39:57 +0200

    fix CI pipeline

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -3,10 +3,10 @@ import numpy as np
 from dotenv import load_dotenv
 import os
 
+PATH = "models/food_classification_model/checkpoints/resnet50_full_dataset.pth"
+
 def test_model_accuracy_threshold():
     load_dotenv()
-    path_to_model = os.getenv("PATH_TO_MODEL")
-    model_data = torch.load(path_to_model)
+    model_data = torch.load(PATH)
     test_acc = model_data.get('test_acc', 0.0)
-
     assert test_acc >= 0.80, f"Top-1 Accuracy too low: {test_acc:.2%}"
